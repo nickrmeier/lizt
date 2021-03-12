224f38340dfe8918ec6b28ca898e34748a0e0885
nickrmeier <nickrmeier@gmaill.com>
2021-03-12 10:12:31 -0800

    google O auth implemented

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy =  require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport')
 const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
+const { cookieKey } = require('./config/keys');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-    }, 
+app.use(
+    cookieSession({
+        // 30 days before cookie expires
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
 
-    (accessToken, refreshToken, profile, done) => {
-        console.log(accessToken);
-        console.log(refreshToken);
-        console.log(profile);
-        console.log(done);
+app.use(passport.initialize());
+app.use(passport.session());
 
-}));
-
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}))
-
-app.get('/auth/google/callback', passport.authenticate('google'))
-
+authRoutes(app)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
